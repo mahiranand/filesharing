d1cd1880660b339bb2abbb3f65b7553f2b1a930e
mahiranand <95206551+mahiranand@users.noreply.github.com>
2023-04-21 16:42:30 +0530

    Add files via upload

---
+++ server/index.js
@@ -0,0 +1,9 @@
+import express from 'express';
+import { router } from './routes/routes.js';
+
+const app = express();
+
+app.use('/',router);
+const PORT = 8000;
+
+app.listen( PORT , () => console.log(`server is running on PORT ${PORT}`));
